aff78f19bc079d12b813015b756cfbe5344e5cd0
simonanderson16 <uhx8bu@virginia.edu>
2024-03-30 15:39:19 -0400

    html to react demo

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import About from "./components/About";
 
 function App() {
-	return (
-		<>
-			<h1>My Website!</h1>
-		</>
-	);
+    return <About />;
 }
 
 export default App;
